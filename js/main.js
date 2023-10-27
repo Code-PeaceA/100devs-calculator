b94232f98f7c18cb845da51eb9025ebc6fe69aa5
Peace Alawode <peace.alawode@hotmail.co.uk>
2023-10-27 22:02:45 +0100

    Updated index.html with id for each button. Created variables and event listeners for inputs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+// Declare variable for calculator output
+let output = document.getElementById('output');
+
+// Declare variables for calculator inputs and add event listeners
+// Numbers
+const num0 = document.getElementById('num0').addEventListener('click', outputPush);
+const num1 = document.getElementById('num1').addEventListener('click', outputPush);
+const num2 = document.getElementById('num2').addEventListener('click', outputPush);
+const num3 = document.getElementById('num3').addEventListener('click', outputPush);
+const num4 = document.getElementById('num4').addEventListener('click', outputPush);
+const num5 = document.getElementById('num5').addEventListener('click', outputPush);
+const num6 = document.getElementById('num6').addEventListener('click', outputPush);
+const num7 = document.getElementById('num7').addEventListener('click', outputPush);
+const num8 = document.getElementById('num8').addEventListener('click', outputPush);
+const num9 = document.getElementById('num9').addEventListener('click', outputPush);
+// Operators
+const divide = document.getElementById('divide').addEventListener('click', outputPush);
+const multiply = document.getElementById('multiply').addEventListener('click', outputPush);
+const add = document.getElementById('add').addEventListener('click', outputPush);
+const decimal = document.getElementById('decimal').addEventListener('click', outputPush);
+const equals = document.getElementById('equals').addEventListener('click', outputPush);
+const subtract = document.getElementById('subtract').addEventListener('click', outputPush);
+
+// when clicked, value should show in output
+
+function outputPush() {
+    let inputValue = [0];
+    
+}
+
 
